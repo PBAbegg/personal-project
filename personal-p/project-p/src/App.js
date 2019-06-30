69b57c6aa924d4c29f768d3765d2cf11921ad6a2
Parker Abegg <pbabegg@gmail.com>
2019-06-30 14:46:34 -0600

    added material-ui card

--- personal-p/project-p/src/App.js
+++ personal-p/project-p/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-function App() {
+const Cards =() => {
   return (
-    <div>
-
-      Hello
-
-    </div>
-  );
+    <MuiThemeProvider>
+  <Card>
+      <CardMedia
+        overlay={<CardTitle title="Mossy Forest" subtitle="Dartmoor, England" />}>
+          <img src="http://www.fubiz.net/wp-content/uploads/2019/01/Neil-Burnell-mystical-02.jpg" alt="" />
+      </CardMedia>
+      <CardText>
+        Wistman's Wood
+        Photo by Neil Burnell
+      </CardText>
+    </Card>
+    </MuiThemeProvider>
+  )
 }
 
-export default App;
+export default Cards;
