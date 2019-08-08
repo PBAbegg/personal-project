a9903ce2d6d216687dcca6aa6b662cefb567d975
Parker Abegg <pbabegg@gmail.com>
2019-08-08 10:39:45 -0600

    added floating label text

--- personal-p/project-p/src/App.js
+++ personal-p/project-p/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
-const Cards =() => {
+const Mui =() => {
   return (
     <MuiThemeProvider>
-  <Card>
-      <CardMedia
-        overlay={<CardTitle title="Mossy Forest" subtitle="Dartmoor, England" />}>
-          <img src="http://www.fubiz.net/wp-content/uploads/2019/01/Neil-Burnell-mystical-02.jpg" alt="" />
-      </CardMedia>
-      <CardText>
-        Wistman's Woods
-        Photo by Neil Burnell
-      </CardText>
-    </Card>
+      <TextField
+        hintText="What's New?"
+        floatingLabelText="New Entry"
+        floatingLabelFixed={true}/>
+        <br/>
+      <Card>
+        <CardMedia
+          overlay={<CardTitle title="Wistman's Woods" subtitle="Dartmoor, England" />}>
+            <img src="http://www.fubiz.net/wp-content/uploads/2019/01/Neil-Burnell-mystical-02.jpg" alt="" />
+        </CardMedia>
+        <CardText>
+          Photo by Neil Burnell
+        </CardText>
+      </Card>
     </MuiThemeProvider>
   )
 }
 
-export default Cards;
+
+export default Mui;
